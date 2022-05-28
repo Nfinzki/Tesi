99033db7738bff962ff44f4ebc77a7e042690654
Nfinzki <francescodiluzio07@gmail.com>
2022-05-28 20:55:17 +0200

    Created second scene

---
+++ Scenario/ExclusiveSpace/src/game.ts
@@ -0,0 +1,93 @@
+// NOTE: remember to add &ENABLE_WEB3 to the url when running locally
+import * as EthereumController from "@decentraland/EthereumController"
+import * as crypto from "@dcl/crypto-scene-utils"
+import { Door } from "./door"
+import { Sound } from "./sound"
+import * as ui from "@dcl/ui-scene-utils"
+import { createWall } from "./entities/wall"
+
+// Config
+let userAddress: string
+
+const contractAddress = "0x6865582a1402F690EA25FDB5d8601bED0Cb0cA43"
+
+// Sounds
+const openDoorSound = new Sound(new AudioClip("sounds/openDoor.mp3"), false)
+const accessDeniedSound = new Sound(new AudioClip("sounds/accessDenied.mp3"), false)
+
+// Music
+const jazzMuffledSound = new Sound(new AudioClip("sounds/jazzMuffled.mp3"), true, true)
+const jazzSound = new Sound(new AudioClip("sounds/jazz.mp3"), true, true)
+jazzSound.getComponent(AudioSource).volume = 0.0
+
+// Base
+const base = new Entity()
+base.addComponent(new GLTFShape("models/baseDarkWithCollider.glb"))
+engine.addEntity(base)
+
+// Facade
+const facade = new Entity()
+facade.addComponent(new GLTFShape("models/facade.glb"))
+facade.addComponent(new Transform({position: new Vector3(4, 0, 8), rotation: Quaternion.Euler(0, 90, 0)}))
+facade.getComponent(Transform).rotate(Vector3.Up(), 180)
+engine.addEntity(facade)
+
+// Walls
+createWall(new Vector3(10, 14, 1), new Vector3(9, 0, 15));
+createWall(new Vector3(10, 14, 1), new Vector3(9, 0, 1));
+createWall(new Vector3(1, 14, 15), new Vector3(14, 0, 8));
+
+
+// Door
+const door = new Door(new GLTFShape("models/door.glb"))
+door.setParent(facade)
+door.addComponent(
+  new OnPointerDown(
+    () => {
+      checkTokens()
+    },
+    {
+      button: ActionButton.PRIMARY,
+      hoverText: "Enter Club",
+      showFeedback: true,
+    }
+  )
+)
+
+// UI
+let noSign = new ui.CenterImage("images/no-sign.png", 1, true, 0, 20, 128, 128, {
+  sourceHeight: 512,
+  sourceWidth: 512,
+  sourceLeft: 0,
+  sourceTop: 0,
+})
+
+// On load
+executeTask(async () => {
+  try {
+    userAddress = await EthereumController.getUserAccount()
+    log("User Address: ", userAddress)
+  } catch (error) {
+    log(error)
+  }
+})
+
+// Check player's wallet to see if they're holding any tokens relating to that contract address
+async function checkTokens() {
+  let balance = await crypto.currency.balance(contractAddress, userAddress)
+
+  log("BALANCE: ", balance)
+
+  if (Number(balance) > 0) {
+    door.playDoorOpen()
+    openDoorSound.getComponent(AudioSource).playOnce()
+    jazzSound.getComponent(AudioSource).volume = 1.0
+  } else {
+    noSign.show(1)
+    accessDeniedSound.getComponent(AudioSource).playOnce()
+    jazzMuffledSound.getComponent(AudioSource).volume = 1.0
+  }
+}
+
+
+
---
+++ Scenario/Gallery/src/entities/labeledNFT.ts
@@ -0,0 +1,96 @@
+import { requirePayment } from "@decentraland/EthereumController";
+import { getProvider } from "@decentraland/web3-provider";
+import {RequestManager, BigNumber, ContractFactory, fromWei } from "eth-connect"
+import NFT_ABI from "src/contracts/NFT_ABI";
+import { ChangedForSale, currentUserAddress, marketplaceAddress, nullAddress, sceneMessageBus } from "src/resources"
+import { acquireNFT } from "src/transactions"
+
+export class LabledNFT {
+    contract: any;
+    forSaleText: TextShape;
+
+    constructor(imagePath: string, position: Vector3, rotation: Quaternion, contractAddress: string, tokenId: number) {
+        executeTask(async () => {
+            const provider = await getProvider();
+            const requestManager = new RequestManager(provider)
+            const factory = new ContractFactory(requestManager, NFT_ABI);
+            const contract = (await factory.at(contractAddress)) as any
+
+            let ownerAddress = await contract.ownerOf(tokenId);
+            
+            const nftOwner = new Entity();
+            const textComponent = new TextShape(ownerAddress);
+            textComponent.fontSize = 1;
+            textComponent.color = Color3.Black();
+            nftOwner.addComponent(textComponent)
+            nftOwner.addComponent(textPosition)
+            engine.addEntity(nftOwner);
+
+            
+
+            // const balanceWei = await requestManager.eth_getBalance(ownerAddress, "latest");
+            // const balance = fromWei(balanceWei, "ether");
+
+            nftImage.addComponent(new OnPointerDown(async (e) => {
+                log("Dati: " + currentUserAddress + "\n" + ownerAddress)
+                await requirePayment("0x30dce1ecc30ca8880eF9BBD2664bE7F2a41D0637", 0.00001, "ETH");
+                if (currentUserAddress.toLocaleLowerCase() === ownerAddress.toLocaleLowerCase()) {
+                    if (!this.forSaleText.visible) {
+                        //Approve Marketplace
+                        log(marketplaceAddress);
+                        log(await contract.approve(marketplaceAddress, tokenId, {from: currentUserAddress}));
+                    } else {
+                        //Revoke approval
+                        await contract.approve(nullAddress, tokenId, {from: currentUserAddress});
+                    }
+
+                    this.forSaleText.visible = !this.forSaleText.visible;
+                    const syncMsg: ChangedForSale = {
+                        forSale: this.forSaleText.visible
+                    }
+                    sceneMessageBus.emit("changedForSale", syncMsg);
+                } else {
+                    //Marketplace.buyNFT
+                    log(await contract.getApproved(tokenId));
+                    // acquireNFT(currentUserAddress, ownerAddress, contractAddress, tokenId, new BigNumber(5), textComponent);
+                }
+            }, {
+                button: ActionButton.POINTER
+            },
+            ))
+        })
+
+        //Create entity and assign shape
+        const nftImage = new Entity()
+        nftImage.addComponent(new PlaneShape())
+        //Create material and configure its fields
+        const myMaterial = new BasicMaterial()
+        myMaterial.texture = new Texture(imagePath)
+        //Assign the material to the entity
+        nftImage.addComponent(myMaterial)
+        nftImage.addComponent(new Transform({
+            position: position,
+            rotation: rotation
+        }))
+        
+        engine.addEntity(nftImage)
+
+        let textPosition = new Transform({ position: position.clone()});
+        textPosition.position.y -= 0.7;
+
+        let forSalePosition = new Transform({position: position.clone()});
+        forSalePosition.position.y += 0.7;
+
+        //Create "For Sale" entity
+        const forSale = new Entity();
+        this.forSaleText = new TextShape("For sale");
+        this.forSaleText.fontSize = 3;
+        this.forSaleText.color = Color3.Green();
+        this.forSaleText.visible = false;
+        forSale.addComponent(this.forSaleText);
+        forSale.addComponent(forSalePosition);
+        engine.addEntity(forSale);
+    }
+
+
+}

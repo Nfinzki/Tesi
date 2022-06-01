9b8aed73ba74fe430bc598ad4991884f6f08e264
Nfinzki <francescodiluzio07@gmail.com>
2022-06-01 23:41:10 +0200

    Added contract addresses

--- Scenario/Gallery/src/resources.ts
+++ Scenario/Gallery/src/resources.ts
@@ -4,7 +4,14 @@ import { getProvider } from "@decentraland/web3-provider"
 
 export const myNFTAddress = "0x6865582a1402F690EA25FDB5d8601bED0Cb0cA43";
 export const marketplaceAddress = "0xd95b0D87b7e5531712702fAa7dD227ff29DFdCd8";
+export const oneTimeBuyNFTAddress = "0xaFb24f93E8724F163c22FEe42d3b772F1143D9a0";
+export const oneTokenNFTAddress = "0xbB59aC8819342d0aF2768715bF86365c95C703bf";
 export const nullAddress = "0x0000000000000000000000000000000000000000";
+
+export const ipfsMyNFT = "ipfs://QmSgNUUUGciW3y5RrzrH3FVvUN4PErd3VUYcVKEwmjW1xB";
+export const ipfsMJ = "ipfs://QmTvWLAtfeq3gg6PokA2BjTNUKxNmEVbtosd845MDBuXCD";
+export const ipfsSax = "ipfs://QmPCVMAG1dqU3rfnZV8TkNLd6CBwLP6cJAd38Yr6vSRDYg";
+export const ipfsTrumpet = "ipfs://QmSvF1mYzRcfZ1Xe46WNz1wQC4E63oCAxW6NxBEaQMkCXG";
 
 export let currentUserAddress: string;
 executeTask(async () => {

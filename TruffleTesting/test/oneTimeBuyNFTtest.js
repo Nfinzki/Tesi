b5540e5924e2f8ac55af16e9a0c80f356a26a2c8
Nfinzki <francescodiluzio07@gmail.com>
2022-07-06 11:16:52 +0200

    Minor changes

--- TruffleTesting/test/oneTimeBuyNFTtest.js
+++ TruffleTesting/test/oneTimeBuyNFTtest.js
@@ -32,7 +32,7 @@ contract("Testing OneTimeBuyNFT", accounts => {
         const nftInstance = await OneTimeBuyNFT.new();
         await nftInstance.mintNFT(accounts[0], "ipfs://QmSgNUUUGciW3y5RrzrH3FVvUN4PErd3VUYcVKEwmjW1xB");
 
-        await nftInstance.safeTransferFrom(accounts[0], accounts[1], 1, {from: accounts[0]});
+        await nftInstance.transferFrom(accounts[0], accounts[1], 1, {from: accounts[0]});
         const owner = await nftInstance.ownerOf(1);
         assert.equal(owner, accounts[1], "The NFT isn't owned by account1");
         
